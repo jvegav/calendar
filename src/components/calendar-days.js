b22515c9f7e675f30a2f9f9b4dc51d931bb028f6
Josue Vega <j.vegav@uniandes.edu.co>
2025-01-22 19:49:30 -0500

    Repository Calendar

---
+++ src/components/calendar-days.js
@@ -0,0 +1,46 @@
+function CalendarDays({ currentDate, ...props }) {
+    let firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    let weekdayOfFirstDay = firstDayOfMonth.getDay();
+
+    let currentDays = [];
+
+    for (let day = 0; day < 42; day++) {
+        if (day === 0 && weekdayOfFirstDay === 0) {
+            firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+        } else if (day === 0) {
+            firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
+        } else {
+            firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+        }
+
+        let calendarDay = {
+            currentMonth: (firstDayOfMonth.getMonth() === currentDate.getMonth()),
+            date: (new Date(firstDayOfMonth)),
+            month: firstDayOfMonth.getMonth(),
+            number: firstDayOfMonth.getDate(),
+            selected: (firstDayOfMonth.toDateString() === currentDate.toDateString()),
+            year: firstDayOfMonth.getFullYear()
+        }
+        console.log(calendarDay.currentMonth)
+
+
+        currentDays.push(calendarDay);
+    }
+
+    return (
+        <div className="grid grid-cols-7 w-full">
+            {
+                currentDays.map((day, index) => {
+                    return (
+                        <div key={index} className={"calendar-day" + (day.currentMonth ? " current" : "") + (day.selected ? " selected" : "") + " w-full h-16 relative border cursor-pointer"}
+                        >
+                            <p className={"absolute right-2 " + (day.selected ? "text-red-600 font-bold" : "") + (day.currentMonth ? "text-black font-bold" : "text-gray-600 font-bold")}  >{day.number} </p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CalendarDays;
---
+++ src/components/calendar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CalendarDays from './calendar-days';
+
+export default class Calendar extends Component {
+
+    constructor() {
+        super();
+        this.weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        this.state = {
+            currentDate: new Date(),
+            currentMonth: new Date()
+        }
+        console.log(this.state.currentDate.getDate());
+
+        this.changeNextMonth = (date) => {
+            this.setState({ currentDate: new Date(date.getFullYear(), date.getMonth() + 1, date.getDay()) });
+        }
+        this.changePastMonth = (date) => {
+            this.setState({ currentDate: new Date(date.getFullYear(), date.getMonth() - 1, date.getDay()) });
+        }
+    }
+    render() {
+        return (
+            <div className="calendar w-full h-full flex flex-col px-20 pt-5">
+                <div className="calendar-header flex flex-col items-center p-4">
+                    <h2 className='text-3xl font-bold cursor-pointer' >{this.months[this.state.currentDate.getMonth()]} {this.state.currentDate.getFullYear()}
+                    </h2>
+                    <button onClick={() => this.changeNextMonth(this.state.currentDate)} >Hola</button>
+                </div>
+                <div className="w-full flex-grow flex flex-col">
+                    <div className="flex items-center justify-around">
+                        {
+                            this.weekdays.map((weekday, index) => {
+                                return <p key={index} className='font-bold w-full text-center'>{weekday}</p>
+                            })
+                        }
+                    </div>
+                    <CalendarDays currentDate={this.state.currentDate} />
+                </div>
+            </div>
+        );
+    }
+}
